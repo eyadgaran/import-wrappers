756652d4b334db7ffdc6920f562952cddb1feab1
Elisha Yadgaran <elishay@alum.mit.edu>
2023-05-06 11:40:36 -0700

    initial setup

---
+++ tests/missing_import_tests.py
@@ -0,0 +1,29 @@
+class TestMissingDependencyImports:
+    """
+    Set of tests to check that the import wrappers work as expected when a dependency is missing.
+    This
+    """
+
+    def test_missing_dependency(self):
+        assert True
+
+
+class TestMissingDependencyUsage:
+    """
+    Set of tests to check that the import wrappers raise appropriate errors when a missing
+    dependency is used.
+    """
+
+    def test_missing_dependency(self):
+        assert True
+
+
+class TestExistingDependencyImports:
+    """
+    Set of tests to check that the import wrappers work as expected when a dependency is present.
+    This focuses on verifying the import mechanism since we cannot use the default import syntax
+    (e.g. from dependency import module)
+    """
+
+    def test_missing_dependency(self):
+        assert True
